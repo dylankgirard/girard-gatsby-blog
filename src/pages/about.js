c76b65be3b4b44b0b4b2545f0a311c6b326b04ff
dylankgirard <dylankgirard@gmail.com>
2021-07-30 15:52:36 -0700

    Nav pages added

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>
+                About Me
+            </h1>
+            <p>I grew up in Oregon, and I also play the trumpet.</p>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>
+                Contact Me!
+            </h1>
+            <p>Want to talk? Reach me at dillyG.net</p>
+        </div>
+    )
+}
+
+export default ContactPage

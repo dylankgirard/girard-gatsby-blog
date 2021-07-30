b2d55b3b4e552465a7ce2daec55655a9e0bd7bd6
dylankgirard <dylankgirard@gmail.com>
2021-07-30 15:42:13 -0700

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hi There,</h1>
+      <h2>I'm Dylan, a boss from LA!</h2>
+    </div>
+  )
 }
+
+export default IndexPage;

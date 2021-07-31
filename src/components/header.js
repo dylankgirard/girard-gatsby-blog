03fd252623920e90cac2e7d4bf7cac1b3c34679c
dylankgirard <dylankgirard@gmail.com>
2021-07-30 22:13:53 -0700

    Added sass modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import * as headerStyles from './header.module.scss'
+
 const Header = () => {
     return (
-        <header>
-            <h1><Link to="/">Dylan Girard - Software Engineer</Link></h1>
-            <nav>
-            <ul>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-            </ul>
-            </nav>
-        </header>
+      <header>
+        <h1>
+          <Link className={headerStyles.link} to="/">
+            DG - Software Engineer
+          </Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from "../components/layout"
 
+
 import { Link } from 'gatsby'
 
 const IndexPage = () => {
@@ -10,8 +11,7 @@ const IndexPage = () => {
       <h1>Hi There,</h1>
       <h2>I'm Dylan, a boss from LA!</h2>
       <p>Need a developer? <Link to="/contact">Contact Me.</Link></p>
-      <p>Learn more <Link to="/about">about</Link> me.</p>
-      <p>Check out my <Link to="/blog">blog.</Link></p>
+      
      
     </Layout>
   )

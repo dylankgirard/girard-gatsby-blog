b736d6bbab4e56595f6b24e54fb4091e4d240306
dylankgirard <dylankgirard@gmail.com>
2021-08-02 23:11:47 -0700

    Rendered blog posts dynamically to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import Layout from '../components/layout'
-
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Layout from "../components/layout"
 
 const BlogPage = () => {
-    return (
-        <Layout>
-            <h1>
-                Blog
-            </h1>
-            <p>Posts will show up here.</p>
-        </Layout>
-    )
+  const blogData = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const blogArr = blogData.allMarkdownRemark.edges
+  
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      <ol>
+        {blogArr.map((blog, i) => (
+          <li key={i}>
+            <h2>{blog.node.frontmatter.title}</h2>
+            <p>{blog.node.frontmatter.date}</p>
+          </li>
+        ))}
+      </ol>
+    </Layout>
+  )
 }
 
 export default BlogPage

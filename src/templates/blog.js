991a019b074f42c6a4bfa44172f3a868777a2ea9
dylankgirard <dylankgirard@gmail.com>
2021-08-10 16:47:00 -0700

    Blog posts now draw data from Contenful project, fixed issues with images not rendering

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,26 +1,54 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../components/layout"
 
 export const query = graphql`
   query ($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            __typename
+            fixed(width: 1600) {
+              width
+              height
+              src
+              srcSet
+            }
+          }
+        }
       }
-      html
     }
   }
 `
 
 const Blog = props => {
+  const imgSrc = props.data.contentfulBlogPost.body.references[0].fixed.src
+
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        const alt = node.data.target
+        const url = imgSrc
+        return <img alt={alt} src={url} />
+      },
+    },
+  }
+
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      {documentToReactComponents(
+        JSON.parse(props.data.contentfulBlogPost.body.raw),
+        options
+      )}
     </Layout>
   )
 }

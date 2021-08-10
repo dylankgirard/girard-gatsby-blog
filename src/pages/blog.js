e210285f95d14c572b4f00dff67536f13ec4fbf1
dylankgirard <dylankgirard@gmail.com>
2021-08-09 19:53:42 -0700

    Blog page now renders Contentful API entries

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,38 +1,34 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
-import * as blogStyles from './blog.module.scss' 
+import * as blogStyles from "./blog.module.scss"
 
 const BlogPage = () => {
   const blogData = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
           }
         }
       }
     }
   `)
 
-  const blogArr = blogData.allMarkdownRemark.edges
-  
+  const blogArr = blogData.allContentfulBlogPost.edges
+
   return (
     <Layout>
       <h1>Blog</h1>
       <ol className={blogStyles.posts}>
         {blogArr.map((blog, i) => (
           <li className={blogStyles.post} key={i}>
-            <Link to={`/blog/${blog.node.fields.slug}`}>
-              <h2>{blog.node.frontmatter.title}</h2>
-              <p>{blog.node.frontmatter.date}</p>
+            <Link to={`/blog/${blog.node.slug}`}>
+              <h2>{blog.node.title}</h2>
+              <p>{blog.node.publishedDate}</p>
             </Link>
           </li>
         ))}

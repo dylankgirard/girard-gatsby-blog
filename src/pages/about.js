3f2a767fd64ca8617b460a3a4a8ad6b5fb607a56
dylankgirard <dylankgirard@gmail.com>
2021-08-11 18:42:19 -0700

    Added a few images

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import Head from "../components/head"
 
-
+import oregon from '../assets/Sunrise-landscape-Mountain-meadow-flower-red-sky-Sparks-Lake-Deschutes-National-Forest-Oregon-USА-1920x1080.jpg'
 import { Link } from 'gatsby'
 
 
@@ -13,7 +13,9 @@ const AboutPage = () => {
             <h1>
                 About Me
             </h1>
-            <p>I grew up in Oregon, and I also play the trumpet.</p>
+
+            <h2>I grew up in Oregon, and I also play the trumpet.</h2>
+            <img src={oregon} alt='Oregon'></img>
             <p>Need to contact me? Go <Link to="/contact">here!</Link></p>
         </Layout>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
 import Head from "../components/head"
+import LA from '../assets/WallpaperDog-975523.jpg'
 
 const IndexPage = () => {
   return (
@@ -9,6 +10,10 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hi There,</h1>
       <h2>I'm Dylan, a software developer from LA!</h2>
+      <img
+        src={LA}
+        alt="Los Angeles"
+      ></img>
       <p>
         Need a developer? <Link to="/contact">Contact Me.</Link>
       </p>

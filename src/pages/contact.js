b71470a42b3c212b4d520126baa9a0263fc5e848
dylankgirard <dylankgirard@gmail.com>
2021-08-31 15:56:02 -0700

    Various styling implementations

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const ContactPage = () => {
   return (
     <Layout>
       <Head title="Contact" />
-      <h1>Contact Me!</h1>
+      <h1>Contact</h1>
       <p>
         Want to talk? Reach me at{" "}
         <Link to="https://www.dylankgirard.com" target="_blank">

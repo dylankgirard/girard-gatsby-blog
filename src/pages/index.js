42ac9194a30030fd436a2625f88e674b1e686932
dylankgirard <dylankgirard@gmail.com>
2021-08-31 15:29:23 -0700

    Added fullscreen video to front page, removed unneeded elements

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,21 @@ import Layout from "../components/layout"
 import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
 import Head from "../components/head"
+import santaMonicaVid from "../assets/motionplaces0035c065q001-480.mp4"
 
-const IndexPage = (props) => {
+const IndexPage = props => {
   return (
     <Layout>
       <Head title="Home" />
-      <h1>Hi There,</h1>
+      <video className="landing-vid" autoPlay muted loop id="santamonica">
+        <source
+          src={santaMonicaVid}
+          type="video/mp4"
+        />
+      </video>
+      {/* <h1>Hi There,</h1>
       <h2>I'm Dylan, a software developer from LA!</h2>
-      <Img fluid={props.data.imageOne.childImageSharp.fluid} />
+      <Img fluid={props.data.imageOne.childImageSharp.fluid} /> */}
     </Layout>
   )
 }

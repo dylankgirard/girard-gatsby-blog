403823e5c052b8b366dd468ccd608555a7c08bce
dylankgirard <dylankgirard@gmail.com>
2021-09-02 12:10:10 -0700

    Small update to form for testing purposes

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -5,10 +5,6 @@ const ContactForm = () => {
   // JSX doesn't like a single </br> tag
   const lineBreak = () => <br></br>
 
-  // const handleSubmit = event => {
-  //   event.preventDefault()
-  // }
-
   return (
     <form
       className={contactStyles.contactform}
@@ -16,7 +12,6 @@ const ContactForm = () => {
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
-      // onSubmit={handleSubmit}
     >
       <input type="hidden" name="form-name" value="contact" />
 
@@ -38,7 +33,7 @@ const ContactForm = () => {
       <input type="textarea" name="message"></input>
       {lineBreak}
 
-      <input type="submit" type="Submit"></input>
+      <input type="submit" name="submit"></input>
     </form>
   )
 }
